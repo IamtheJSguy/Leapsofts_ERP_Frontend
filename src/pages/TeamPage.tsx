26b57d50c47b178195d43e0d8a7567dc703d3848
Ahmad <ahmad00suleman000@gmail.com>
2026-09-23 22:14:45 +0500

    feat: add admin reset password frontend changes

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -897,24 +897,28 @@ const TeamPage = () => {
                 Edit
               </Button>
 
-              {isAdmin && (
-                <>
-                  <Button
-                    startIcon={<LockIcon sx={{ fontSize: 15 }} />}
-                    sx={actionButtonSx}
-                    onClick={() => setIsReset2faOpen(true)}
-                  >
-                    Reset 2FA
-                  </Button>
-                  <Button
-                    startIcon={<LockIcon sx={{ fontSize: 15 }} />}
-                    sx={actionButtonSx}
-                    onClick={() => setIsResetPasswordOpen(true)}
-                  >
-                    Reset Password
-                  </Button>
-                </>
-              )}
+              {(() => {
+                const baseOrgId = selectedUser.baseOrganizationId || selectedUser.organizationId;
+                const isBaseOrgAdmin = isAdmin && baseOrgId === currentUser?.organizationId;
+                return isBaseOrgAdmin && (
+                  <>
+                    <Button
+                      startIcon={<LockIcon sx={{ fontSize: 15 }} />}
+                      sx={actionButtonSx}
+                      onClick={() => setIsReset2faOpen(true)}
+                    >
+                      Reset 2FA
+                    </Button>
+                    <Button
+                      startIcon={<LockIcon sx={{ fontSize: 15 }} />}
+                      sx={actionButtonSx}
+                      onClick={() => setIsResetPasswordOpen(true)}
+                    >
+                      Reset Password
+                    </Button>
+                  </>
+                );
+              })()}
 
               <Button
                 startIcon={<DeleteIcon sx={{ fontSize: 15 }} />}
