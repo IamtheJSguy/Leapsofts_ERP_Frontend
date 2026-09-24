c2667a93d97225c95cffe70c5adc243bca34df2d
Ahmad <ahmad00suleman000@gmail.com>
2026-09-24 14:30:45 +0500

    hide 2fa reset

--- src/pages/TeamPage.tsx
+++ src/pages/TeamPage.tsx
@@ -898,8 +898,8 @@ const TeamPage = () => {
               </Button>
 
               {(() => {
-                const baseOrgId = selectedUser.baseOrganizationId || selectedUser.organizationId;
-                const isBaseOrgAdmin = isAdmin && baseOrgId === currentUser?.organizationId;
+                const isBaseOrgAdmin =
+                  isAdmin && selectedUser.baseOrganizationId === currentUser?.organizationId;
                 return isBaseOrgAdmin && (
                   <>
                     <Button
